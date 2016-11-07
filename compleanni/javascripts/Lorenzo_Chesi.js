2f198995c831470e7f88aea239f8efb664aec72f
Cheter01 <lorenzo.chesi@live.it>
2016-11-07 19:37:47 +0100

    Update Lorenzo_Chesi.js

--- compleanni/javascripts/Lorenzo_Chesi.js
+++ compleanni/javascripts/Lorenzo_Chesi.js
@@ -23,3 +23,394 @@ function Lorenzo_Chesi() {
        
         return false;
     };
+
+function Simone_Palai() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Simone_Palai").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Simone Palai"
+        document.getElementById("compleanno").innerHTML = "14 Gennaio"
+       
+        return false;
+    };
+
+function Edison_Dantas() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Edison_Dantas").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Edison Dantas"
+        document.getElementById("compleanno").innerHTML = "25 Gennaio"
+       
+        return false;
+    };
+
+function Samuele_Varriale() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Samuele_Varriale").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Samuele Varriale"
+        document.getElementById("compleanno").innerHTML = "30 Gennaio"
+       
+        return false;
+    };
+
+function Francesco_Del_Mondo() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Francesco_Del_Mondo").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Francesco Del Mondo"
+        document.getElementById("compleanno").innerHTML = "18 Maggio"
+       
+        return false;
+    };
+
+function Chiara_Di_Cesare() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Chiara_Di_Cesare").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Chiara Di Cesare"
+        document.getElementById("compleanno").innerHTML = "15 Giugno"
+       
+        return false;
+    };
+
+function Francesco_Frassi() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Francesco_Frassi").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Francesco Frassi"
+        document.getElementById("compleanno").innerHTML = "21 Giugno"
+       
+        return false;
+    };
+
+function Sara_Fiorentini() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Sara_Fiorentini").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Sara Fiorentini"
+        document.getElementById("compleanno").innerHTML = "23 Agosto"
+       
+        return false;
+    };
+
+function Paolo_Lucchesi() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Paolo_Lucchesi").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Paolo Lucchesi"
+        document.getElementById("compleanno").innerHTML = "13 Settembre"
+       
+        return false;
+    };
+
+function Sara_Papaleo() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Sara_Papaleo").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Sara Papaleo"
+        document.getElementById("compleanno").innerHTML = "8 Ottobre"
+       
+        return false;
+    };
+
+function Lura_Genua() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Lura_Genua").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Laura Genua"
+        document.getElementById("compleanno").innerHTML = "8 Novembre"
+       
+        return false;
+    };
+
+function Martina_Gnudi() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Martina_Gnudi").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Martina Gnudi"
+        document.getElementById("compleanno").innerHTML = "15 Novembre"
+       
+        return false;
+    };
+
+function Dennis_Bonaguidi() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Dennis_Bonaguidi").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Dennis Bonaguidi"
+        document.getElementById("compleanno").innerHTML = "3 Dicembre"
+       
+        return false;
+    };
+
+function Leonardo_Prosperi() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Leonardo_Prosperi").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Leonardo Prosperi"
+        document.getElementById("compleanno").innerHTML = "13 Dicembre"
+       
+        return false;
+    };
+
+function Samy_Abd_Al_Amer() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Samy_Abd_Al_Amer").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Samy Abd Al Amer"
+        document.getElementById("compleanno").innerHTML = "18 Dicembre"
+       
+        return false;
+    };
+
+function Francesco_Mottola() {
+        var bar = $('.progress-bar span');
+          
+         var now = new Date();
+            var start = new Date(now.getFullYear(), 0, 0);
+            var diff = now - start;
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            var compl = document.getElementById("bt_Francesco_Mottola").value|0;
+            
+            if (day > compl) {
+              var value = ((day - compl) / 365) * 100;            
+              var giorni = (365 - day) + compl;
+            } else {
+              var value = ((day + 365 - compl) / 365) * 100;
+              var giorni = compl - day;
+            }
+   
+        bar.css('width', value + '%');
+        document.getElementById("label").innerHTML = giorni + " Giorni";
+        document.getElementById("Titolo").innerHTML = "Francesco Mottola"
+        document.getElementById("compleanno").innerHTML = "?? ??"
+       
+        return false;
+    };
+
