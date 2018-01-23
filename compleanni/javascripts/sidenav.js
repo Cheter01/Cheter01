3fcfcc7082f322a7614c8f213bfc359fd7d03e19
Cheter01 <lorenzo.chesi@live.it>
2018-01-23 21:25:49 +0100

    oky

---
+++ compleanni/javascripts/sidenav.js
@@ -0,0 +1,22 @@
+function sidenav() {
+  var nav = document.getElementById("mySidenav");
+
+  if (nav.style.height === "150px") {
+        nav.style.height = "0";
+      } else {
+        nav.style.height = "150px";
+      }
+}
+
+
+
+
+// function openNav() {
+//     document.getElementById("mySidenav").style.height = "200px";
+//     // document.getElementById("main").style.marginTop = "350px";
+// }
+//
+// function closeNav() {
+//     document.getElementById("mySidenav").style.height = "0";
+//     // document.getElementById("main").style.marginTop = "0";
+// }
