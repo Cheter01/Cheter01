1f201335d30b2f3b6e24f39fc1c45f0015d05438
Cheter01 <lorenzo.chesi@live.it>
2018-07-13 23:30:41 +0200

    fg

--- js/compleanni.js
+++ js/compleanni.js
@@ -1,19 +1,19 @@
 
-var value=20;
+// var value=20;
+var person = [
+{nome:"Edison", cognome:"Dantas", giorno_su_anno:25, giorno_su_mese:25, mese:"Gennaio", anno:2001, email:"edison.25012001@gmail.com", sesso: "Maschio"},
+{nome:"Lorenzo", cognome:"Chesi", giorno_su_anno:57, giorno_su_mese:26, mese:"Febbraio", anno:2001, email:"lorenzo.chesi@live.it", sesso: "Maschio"},
+{nome:"Francesco", cognome:"Del Mondo", giorno_su_anno:138, giorno_su_mese:18, mese:"Maggio", anno:2001, email:"Francethebest2001@gmail.com", sesso: "Maschio"},
+{nome:"Chiara", cognome:"Di Cesare", giorno_su_anno:166, giorno_su_mese:15, mese:"Giugno", anno:2001, email:"chiaradicesare001@gmail.com"},
+{nome:"Francesco", cognome:"Frassi", giorno_su_anno:172, giorno_su_mese:21, mese:"Giugno", anno:2001, email:"Skynetfrafra.FF@gmail.com", sesso: "Maschio"},
+{nome:"Paolo", cognome:"Lucchesi", giorno_su_anno:256, giorno_su_mese:13, mese:"Settembre", anno:2001, email:"lucchesipaolo22@yahoo.it", sesso: "Maschio"},
+{nome:"Sara", cognome:"Papaleo", giorno_su_anno:281, giorno_su_mese:8, mese:"Ottobre", anno:2001, email:"saraffaele@alice.it"},
+{nome:"Laura", cognome:"Genua", giorno_su_anno:313, giorno_su_mese:8, mese:"Novembre", anno:2001, email:"lauragenua01@gmail.com"},
+{nome:"Francesco", cognome:"Mottola", giorno_su_anno:320, giorno_su_mese:15, mese:"Novembre", anno:2001, email:"", sesso: "Maschio"},
+{nome:"Martina", cognome:"Gnudi", giorno_su_anno:320, giorno_su_mese:15, mese:"Novembre", anno:2000, email:"giulia_gnudi91@hotmail.it"},
+{nome:"Dennis", cognome:"Bonaguidi", giorno_su_anno:337, giorno_su_mese:3, mese:"Dicembre", anno:2001, email:"dennis.bonaguidi@gmail.com", sesso: "Maschio"},
+{nome:"Leonardo", cognome:"Prosperi", giorno_su_anno:347, giorno_su_mese:13, mese:"Dicembre", anno:2001, email:"", sesso: "Maschio"}];
 
-var person1 = {nome:"Edison", cognome:"Dantas", giorno_su_anno:25, giorno_su_mese:25, mese:"Gennaio", anno:2001, email:"edison.25012001@gmail.com"};
-var person2 = {nome:"Lorenzo", cognome:"Chesi", giorno_su_anno:57, giorno_su_mese:26, mese:"Febbraio", anno:2001, email:"lorenzo.chesi@live.it"};
-var person3 = {nome:"Francesco", cognome:"Del Mondo", giorno_su_anno:138, giorno_su_mese:18, mese:"Maggio", anno:2001, email:"Francethebest2001@gmail.com"};
-var person4 = {nome:"Chiara", cognome:"Di Cesare", giorno_su_anno:166, giorno_su_mese:15, mese:"Giugno", anno:2001, email:"chiaradicesare001@gmail.com"};
-var person5 = {nome:"Francesco", cognome:"Frassi", giorno_su_anno:172, giorno_su_mese:21, mese:"Giugno", anno:2001, email:"Skynetfrafra.FF@gmail.com"};
-var person6 = {nome:"Paolo", cognome:"Lucchesi", giorno_su_anno:256, giorno_su_mese:13, mese:"Settembre", anno:2001, email:"lucchesipaolo22@yahoo.it"};
-var person7 = {nome:"Sara", cognome:"Papaleo", giorno_su_anno:281, giorno_su_mese:8, mese:"Ottobre", anno:2001, email:"saraffaele@alice.it"};
-var person8 = {nome:"Laura", cognome:"Genua", giorno_su_anno:313, giorno_su_mese:8, mese:"Novembre", anno:2001, email:"lauragenua01@gmail.com"};
-var person9 = {nome:"Francesco", cognome:"Mottola", giorno_su_anno:320, giorno_su_mese:15, mese:"Novembre", anno:2001, email:""};
-var person10 = {nome:"Martina", cognome:"Gnudi", giorno_su_anno:320, giorno_su_mese:15, mese:"Novembre", anno:2001, email:"giulia_gnudi91@hotmail.it"};
-var person11 = {nome:"Dennis", cognome:"Bonaguidi", giorno_su_anno:337, giorno_su_mese:3, mese:"Dicembre", anno:2001, email:"dennis.bonaguidi@gmail.com"};
-var person12 = {nome:"Leonardo", cognome:"Prosperi", giorno_su_anno:347, giorno_su_mese:13, mese:"Dicembre", anno:2001, email:""};
-var person = [person1, person2, person3, person4, person5, person6, person7, person8, person9, person10, person11, person12];
 
 
 function myFunction(){
@@ -26,21 +26,15 @@ function myFunction(){
             var diff = now - start; //tempo dall'inizio dell'anno ad ora (in millisecondi)
             var oneDay = 1000 * 60 * 60 * 24;
             var day = Math.floor(diff / oneDay); //Conversione dei millisecondi in giorni
-            var anni = new Date().getFullYear() - 2001;
             var numero_persone = person.length; //numero persone
 
             for(i=1; i==numero_persone; i++){
 
-							var num1=i;
-							var num2=i+1;
-							var persona1 = "persona" + (num1).toString();
-							var persona2 = "persona" + (num2).toString();
-
             	do{
-            		var compl1 = parseInt(giorni_da_1_1[i]);
-            		var compl2 = parseInt(giorni_da_1_1[i+1]);
+            		var compl1 = parseInt(person[i].giorno_su_anno);
+            		var compl2 = parseInt(person[i+1].giorno_su_anno);
 
-            		if (day > compl) {
+            		if (day > compl1) {
             			var value = ((day - compl1) / 365) * 100;
             			var giorni1 = (365 - day) + parseInt(compl1);
             			var giorni2 = (365 - day) + parseInt(compl2);
@@ -51,18 +45,23 @@ function myFunction(){
             		}
             	}while (giorni1<giorni2);
             }
-            if (giorni <= 0) {
-              document.getElementById("compleanno").innerHTML = "Auguri " + nome +", per i tuoi " + anni  + " anni!";
-              document.getElementById("myimg").src = "images/cake.png";
-              document.getElementById("label").innerHTML = "🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂";
+            // if (giorni <= 0) {
+            //   document.getElementById("compleanno").innerHTML = "Auguri " + nome +", per i tuoi " + anni  + " anni!";
+            //   document.getElementById("myimg").src = "images/cake.png";
+            //   document.getElementById("label").innerHTML = "🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂🎂";
+						//
+            // } else {
 
-            } else {
-            	document.getElementById("date").innerHTML = giorno[i] + " " + String(mese_str[i]);
-							document.getElementById("date").innerHTML = giorno[i] + " " + String(mese_str[i]);
-            	document.getElementById("myimg").src = "images/Classe/"+nome[i]+"_"+cognome[i]+".jpg";
-            	document.getElementById("FAB-perc").innerHTML = giorni1 + " Giorni";
-								document.getElementById("name").innerHTML = nome[i] + " "+ cognome[i];
-            }
+							var anni = new Date().getFullYear() - person[i].anno; //calcolo anni
+
+							document.getElementById("name").innerHTML = person[i].nome + " " + person[i].cognome;
+							document.getElementById("date").innerHTML = person[i].giorno_su_mese + " " + String(person[i].mese) + " " + String(person[i].anno);
+							document.getElementById("sesso").innerHTML = person[i].sesso;
+							document.getElementById("email").innerHTML = person[i].email;
+							document.getElementById("anni").innerHTML = anni;
+            	document.getElementById("img-compl").style.backgroundImage = "url(images/Classe/"+person[i].nome+"_"+person[i].cognome+".jpg";
+            	document.getElementById("FAB-perc").innerHTML = giorni1;
+            // }
         };
 
 				// document.querySelector('#p1').addEventListener('mdl-componentupgraded', function() {
